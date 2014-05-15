4828b205db4ee17a6b6802b080f970766c31ea8e
Justin Smith <noisesmith@gmail.com>
2014-05-15 13:30:30 -0700

    updated debugging functionality

--- src/main/java/org/noisesmith/noisegenerator/DebugBytes.java
+++ src/main/java/org/noisesmith/noisegenerator/DebugBytes.java
@@ -1,6 +1,11 @@
 package org.noisesmith.noisegenerator;
 
-import java.nio.file.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public final class DebugBytes {
     public static void toFile ( String location, byte[] bytes ) {
@@ -12,5 +17,13 @@ public final class DebugBytes {
             e.printStackTrace();
         }
     }
+    public static void toFile ( String location, double[] doubles ) {
+        byte[] buffer = new byte[doubles.length * 2];
+        ByteBuffer out = ByteBuffer.wrap(buffer);
+        out.order(ByteOrder.LITTLE_ENDIAN);
+        for(int i = 0; i < doubles.length; i++) {
+            out.putShort((short) (Math.floor(doubles[i]*Short.MAX_VALUE)));
+        }
+        toFile(location, buffer);
+    }
 }
-
